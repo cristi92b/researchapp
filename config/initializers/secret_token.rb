e869a7d34bbc0dbbf8671715be2ae7343359cce5
Cristi B <cristi92b@gmail.com>
2013-12-08 12:51:37 +0200

    Solved "internal server error" problem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Researchapp::Application.config.secret_token = '39c639ec6a2e4ed34cf34e7317db910c0d76431115f4dd3e2654fe339ebd516c91f679d8c791a21dd993863818861ee27f68298d660e0e7ebe63df9fdabc2bb6'
